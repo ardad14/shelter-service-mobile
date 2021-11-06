272a785796dbf89500eada76635e6e3c13610756
Artem Kuptsov <artem.kuptsov@nure.ua>
2021-11-06 15:24:59 +0200

    Create update shelter with API request

--- app/src/main/java/com/example/petsfromshelter/ShelterActivity.kt
+++ app/src/main/java/com/example/petsfromshelter/ShelterActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.petsfromshelter.entity.Shelter
-import com.example.petsfromshelter.retrofit.objects.GetShelters;
+import com.example.petsfromshelter.models.Shelter
+import com.example.petsfromshelter.retrofit.objects.GetAllShelters;
 import com.example.petsfromshelter.retrofit.objects.UpdateShelter
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,13 +27,26 @@ class ShelterActivity : AppCompatActivity() {
 
 
 
-        val ShelterService = GetShelters.sheltersRetrofitService;
+        val ShelterService = GetAllShelters.sheltersRetrofitService;
         ShelterService.getShelterList().enqueue(object : Callback<ArrayList<Shelter>> {
             override fun onFailure(call: Call<ArrayList<Shelter>>, t: Throwable) {}
 
             override fun onResponse(call: Call<ArrayList<Shelter>>, response: Response<ArrayList<Shelter>>) {
+                var shelter = Shelter(
+                        1,
+                        "name",
+                        "email",
+                        4.4,
+                        5.5,
+                        "phone",
+                        "siteUrl"
+                );
+                response.body()?.forEach {
+                    if (it.id == 2) {
+                        shelter = it;
+                    }
+                }
 
-                val shelter = response.body()?.get(0);
                 //println(shelter)
 
                 name.text.insert(0, shelter?.name.toString())
@@ -53,19 +66,6 @@ class ShelterActivity : AppCompatActivity() {
         val phone: EditText = findViewById<EditText>(R.id.phoneText)
         val email: EditText = findViewById<EditText>(R.id.emailText)
         val siteUrl: EditText = findViewById<EditText>(R.id.urlText)
-
-        /*if (!nameRegex.matches(name.text.toString())) {
-        name.setBackgroundColor(Color.parseColor("#FF0000"))
-        return
-    }
-    if(!phoneRegex.matches(phone.text.toString())) {
-        phone.setBackgroundColor(Color.parseColor("#FF0000"));
-        return
-    }
-    if(!emailRegex.matches(email.text.toString())) {
-        email.setBackgroundColor(Color.parseColor("#FF0000"));
-        return
-    }*/
 
         val shelter = Shelter(
             1,
