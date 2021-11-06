391deb9318ac88047a796a8c78615cc557ce1682
Artem Kuptsov <artem.kuptsov@nure.ua>
2021-11-06 15:23:39 +0200

    Create login with API request

--- app/src/main/java/com/example/petsfromshelter/MainActivity.kt
+++ app/src/main/java/com/example/petsfromshelter/MainActivity.kt
@@ -6,39 +6,44 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.petsfromshelter.models.User
+import com.example.petsfromshelter.models.UserLoginForm
+import com.example.petsfromshelter.retrofit.objects.Login
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+/*
         val shelterActivity = Intent (this, ShelterActivity::class.java)
-        startActivity(shelterActivity)
+        startActivity(shelterActivity)*/
 
     }
 
     fun signUp(view: View) {
         val email: EditText = findViewById<EditText>(R.id.editTextTextEmailAddress)
         val password: EditText = findViewById<EditText>(R.id.editTextTextPassword)
-        val db = FirebaseFirestore.getInstance()
 
-        db.collection("user")
-                .get().addOnSuccessListener {
-                    it.forEach {
-                        if (it.get("email").toString().equals(email.text.toString())) {
-                            if (it.get("password").toString().equals(password.text.toString())) {
-                                val createShelterActivity = Intent (this, CreateShelterActivity::class.java)
-                                startActivity(createShelterActivity)
-                            } else {
-                                password.setBackgroundColor(Color.parseColor("#FF0000"))
-                            }
-                        }
+        val user = UserLoginForm(
+                email.text.toString(),
+                password.text.toString()
+        );
+        val intent = Intent(this, ShelterActivity::class.java)
+        val loginService = Login.login;
+        loginService.login(user).enqueue(object : Callback<User> {
+            override fun onFailure(call: Call<User>, t: Throwable) {}
 
-                    }
-                }
-        email.setBackgroundColor(Color.parseColor("#FF0000"))
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                println(response.body());
+                startActivity(intent);
+            }
+        })
+
 
     }
 
