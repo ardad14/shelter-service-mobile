a3d9bd97762dfe8c71c8953e817f1240b2e81a17
Artem Kuptsov <artem.kuptsov@nure.ua>
2021-11-30 23:52:18 +0200

    Create rendering shelter fragment

--- app/src/main/java/com/example/petsfromshelter/fragments/ShelterInformationFragment.kt
+++ app/src/main/java/com/example/petsfromshelter/fragments/ShelterInformationFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.petsfromshelter.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.petsfromshelter.models.Shelter
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +16,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ShelterInformationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var shelter: Shelter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shelter_information, container, false)
+        arguments?.let {
+            shelter = it.getSerializable("shelter") as Shelter?
+        }
+
+        val view = inflater.inflate(R.layout.fragment_shelter_information, container, false)
+        val name: TextView = view.findViewById<TextView>(R.id.nameText)
+        val phone: TextView = view.findViewById<TextView>(R.id.phoneText)
+        val email: TextView = view.findViewById<TextView>(R.id.emailText)
+        val siteUrl: TextView = view.findViewById<TextView>(R.id.urlText)
+
+        name.text = shelter?.name
+        phone.text = shelter?.phone
+        email.text = shelter?.email
+        siteUrl.text = shelter?.siteUrl
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShelterInformationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShelterInformationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }*/
 }
